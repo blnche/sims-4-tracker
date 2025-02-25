4c62ce6777f294896830843b1e5da16b0c2a3b15
blnche <peltierblanche@gmail.com>
2025-02-25 15:40:58 +0100

    working on dynamic render on modal for new events

--- src/app/account/components/EventModal.tsx
+++ src/app/account/components/EventModal.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 interface ModalProps {
     isOpen: boolean;
@@ -12,23 +12,90 @@ interface ModalProps {
 export default function EventModal({ isOpen, onClose, eventType } : ModalProps) {
     if(!isOpen) return null
     console.log(eventType)
-
+    const type = eventType?.toLowerCase()
+    console.log(type)
     // this function needs arguments : isopen, onclose, eventype, cycle, household id
 
     //each event card has a button to open pop up, each pop up is different in content but design is the same : title design,close button, save button, only the sim selectors varies. 
     //button could be a save function that pass arguments non mandatory 
+    // if event is baby attempt found sim baby attempts number and update it and setBabyAttempt()
+
+    const [babyAttempts, setBabyAttempts] = useState(0)
+
     const modalStrings = {
         intro: [
             {
                 type: 'birth',
-                string: 'A new Sim takes their first breath under the watchful gaze of the Great Watcher.'
+                string1: 'A new Sim takes their first breath under the watchful gaze of the Great Watcher.',
+                string2: 'Welcome,',
+                connector: 'child of…',
+                string3: '',
+                string4: 'Congratulations!'
             },
             {
                 type: 'divorce',
-                string: 'The Great Watcher turns the page of fate… '
-            }
+                string1: 'The Great Watcher turns the page of fate…',
+                string2: 'The bond once forged between',
+                connector: 'and',
+                string3: 'is no more.',
+                string4: 'What was once entwined is now set free—may destiny guide them on new paths.'
+            },
+            {
+                type: 'engagement',
+                string1: 'Under the watchful gaze of the Great Watcher, a promise is made…',
+                string2: '',
+                connector: 'and',
+                string3: 'are now engaged!',
+                string4: 'May their union be blessed.'
+            },
+            {
+                type: 'baby attempt',
+                string1: 'The Great Watcher raises an eyebrow…',
+                string2: '',
+                connector: 'and',
+                string3: 'have set their sights on expanding the family.',
+                string4: 'Will the stars align?',
+                bonus: (attemptsLeft : Number) => `Sim 1 has ${attemptsLeft} attempts left.`
+            },
+            {
+                type: 'marriage',
+                string1: 'By divine decree of the Great Watcher, let it be known—',
+                string2: '',
+                connector: 'and',
+                string3: 'are now united in sacred bond!',
+                string4: 'May their love endure beyond time itself!'
+            },
+            {
+                type: 'coronation',
+                string1: 'With the Great Watcher’s gaze upon them,',
+                string2: '',
+                connector: '',
+                string3: 'ascends to the throne.',
+                string4: 'Let the world witness their reign, for destiny has chosen its ruler!'
+            },
+            {
+                type: 'death',
+                string1: 'The Great Watcher’s gaze faltered, and with it,',
+                string2: '',
+                connector: '',
+                string3: 'was lost.',
+                string4: 'No warning, no fanfare—just a quick exit, leaving everyone thinking, "That was... unexpected."'
+            },
+            {
+                type: 'pregnancy',
+                string1: 'The Great Watcher has foreseen new life on the horizon…',
+                string2: 'A child is on the way for',
+                connector: 'and',
+                string3: '',
+                string4: 'May their journey to parenthood be blessed!'
+            } 
         ]
     }
+
+
+    modalStrings.intro.map((i) => {
+        console.log(i)
+    })
 
     useEffect(() => {
         if(typeof window !== 'undefined') {
@@ -80,10 +147,13 @@ export default function EventModal({ isOpen, onClose, eventType } : ModalProps) 
                     height={96}
                 />
                 {/* changes depending of event */}
-                <p>{modalStrings.intro.find((intro) => intro.type === eventType?.toLowerCase())?.string}</p>
-                <p className="text-center">The Great Watcher's gaze faltered, and with it,</p>
-
-                {/* if divorces or birth one more sentence before everything */}
+                <p className="text-center">{modalStrings.intro.find((intro) => intro.type === type)?.string1}</p>
+                { (type === 'divorce' || type === 'birth' || type === 'pregnancy') && (
+                    <p className="text-center">{modalStrings.intro.find((intro) => intro.type === type)?.string2}</p>
+                )
+                    
+                }
+                {/* if divorces or birth or pregnancy one more sentence before everything */}
                 {/* if divorce, engagement, pregancy oen sentence before selects */}
 
                 {/* if birth : input name input lastname, sentence, select parent 1 select parent 2 */}
@@ -91,12 +161,16 @@ export default function EventModal({ isOpen, onClose, eventType } : ModalProps) 
                 {/* else sim 1 select, 'and', sim 2 select */}
                 <div className="space-y-[20px]">
                     <p>selectsim</p>
-                    <p className="text-center">was lost.</p>
+                    <p className="text-center">{modalStrings.intro.find((intro) => intro.type === type)?.string3}</p>
                 </div>
 
-                {/* changes depending on event */}
-                <p className="text-center">No warning, no fanfare—just a quick exit, leaving everyone thinking, 'That was... unexpected.'</p>
 
+                {/* changes depending on event */}
+                <p className="text-center">{modalStrings.intro.find((intro) => intro.type === type)?.string4}</p>
+
+                {type === 'baby attempt' && (
+                    <p className="text-center text-sm">{modalStrings.intro.find((intro) => intro.type === type)?.bonus(babyAttempts)}</p>
+                )}
                 {/* if baby attempt one more sentence before button */}
                 <button
                     className="w-fit border-[2px] rounded-[14px] px-[25px] py-[10px]"
